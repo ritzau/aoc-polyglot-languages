8f13f4134413a333b4da22b7dafad89ab521292e
Tobias Ritzau <tobias@ritzau.se>
2025-08-19 11:50:41 +0200

    Add nix build support to C language flake using just build

--- languages/c/flake.nix
+++ languages/c/flake.nix
@@ -12,22 +12,56 @@
   };
 
   outputs = { self, nixpkgs, flake-utils, base }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        baseLib = base.lib.${system};
-      in
-      {
-        devShells.default = baseLib.mkLanguageShell {
-          name = "C";
-          emoji = "🔧";
-          languageTools = with baseLib.pkgs; [
-            gcc
-            clang
-            gnumake
-            gdb
-            pkg-config
-            clang-tools # includes clang-format and clang-tidy
-          ];
-        };
-      });
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          baseLib = base.lib.${system};
+        in
+        {
+          devShells.default = baseLib.mkLanguageShell {
+            name = "C";
+            emoji = "🔧";
+            languageTools = with baseLib.pkgs; [
+              gcc
+              clang
+              gnumake
+              gdb
+              pkg-config
+              clang-tools # includes clang-format and clang-tidy
+            ];
+          };
+
+          # Export mkSolution for C solutions to use
+          lib = {
+            # Wrapper that provides the language name automatically
+            mkSolution = args: baseLib.mkSolution ({
+              language = "c";
+              languageFlake = self;
+            } // args);
+            inherit (baseLib) pkgs;
+          };
+        }) // {
+      # Complete solution outputs - eliminates all boilerplate
+      mkStandardOutputs = args: flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = self.lib.${system}.pkgs;
+          # Default package that uses just build
+          defaultPackage = pkgs.stdenv.mkDerivation {
+            pname = "hello-c";
+            version = "0.1.0";
+            src = ./.;
+            nativeBuildInputs = with pkgs; [ just gcc gnumake ];
+            buildPhase = ''
+              just build
+            '';
+            installPhase = ''
+              mkdir -p $out/bin
+              find . -name "hello-c" -executable -type f -exec cp {} $out/bin/ \;
+            '';
+          };
+        in
+        self.lib.${system}.mkSolution ({
+          package = defaultPackage;
+        } // args));
+    };
 }
--- solutions/hello/c/flake.nix
+++ solutions/hello/c/flake.nix
@@ -2,31 +2,9 @@
   description = "Hello World C";
 
   inputs = {
-    base.url = "path:/Users/ritzau/src/slask/aoc-nix/solutions/base";
     c-lang.url = "path:/Users/ritzau/src/slask/aoc-nix/languages/c";
   };
 
-  outputs = { self, base, c-lang }:
-    base.inputs.flake-utils.lib.eachDefaultSystem (system:
-      let
-        baseLib = base.lib.${system};
-        package = baseLib.pkgs.stdenv.mkDerivation {
-          pname = "hello-c";
-          version = "0.1.0";
-          src = ./.;
-          nativeBuildInputs = [ baseLib.pkgs.gcc baseLib.pkgs.gnumake ];
-          buildPhase = ''
-            make
-          '';
-          installPhase = ''
-            install -D hello-c $out/bin/hello-c
-          '';
-        };
-      in
-      baseLib.mkSolution {
-        language = "c";
-        description = "Hello World C";
-        languageFlake = c-lang;
-        package = package;
-      });
+  outputs = { self, c-lang }:
+    c-lang.mkStandardOutputs { };
 }
